7f6bbe779329742498734156b60e49a4cc799ccb
hayyyansaleh6-commits <hayyyansaleh6@gmail.com>
2026-07-02 03:26:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message, "أهلاً بك في Danny_sh 🛍️")
+
+print("Bot is running...")
+bot.infinity_polling
